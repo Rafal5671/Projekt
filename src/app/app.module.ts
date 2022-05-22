6226b5befad542ddbdabb9f1dba8156deaa541c6
Rafal5671 <rafalowczarek567@gmail.com>
2022-05-22 16:09:02 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HarmonogramComponent } from './harmonogram/harmonogram.component';
 
@@ -18,8 +18,7 @@ import { HarmonogramComponent } from './harmonogram/harmonogram.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
