814a4a6c09f97eba3765f5af0581176500a1d365
Rafal5671 <rafalowczarek567@gmail.com>
2022-06-12 19:06:22 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 import { ChildrenOutletContexts, RouterOutlet} from '@angular/router';
-import { /*slider,*//*transformer,*//* fader*/slideInAnimation} from './rout-animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations:[
-    // fader,
-     //slider,
-    //transformer,
-    slideInAnimation
   ]
 })
 export class AppComponent {
   constructor(private contexts: ChildrenOutletContexts) {}
-  getRouteAnimationData() {
-    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
-  }
 }
 
